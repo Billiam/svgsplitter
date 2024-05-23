5f67447807a04872000cb5c706dbb289fa575c3f
Billiam <billiamthesecond@gmail.com>
2024-05-23 17:43:11 -0500

    Write file output

--- src/svgsplitter/splitter.cr
+++ src/svgsplitter/splitter.cr
@@ -1,13 +1,67 @@
+require "xml"
+
 module Svgsplitter
   class Splitter
     def initialize(input_path : String, output_path : String | Nil = nil, prefix : String | Nil = "")
-      @input = input_path
-      @output = output_path
+      @input_path = input_path
+      @output_path = output_path
       @prefix = prefix
     end
 
     def run(dry_run : Bool = false)
-      raise ACON::Exceptions::ValidationFailed.new("SVG (#{@input}) could not be read") unless File.readable? @input
+      raise ACON::Exceptions::ValidationFailed.new("SVG (#{@input_path}) could not be read") unless File.readable? @input_path
+
+      input_path = Path[@input_path]
+      xml = File.read @input_path
+
+      output_directory = if output_param = @output_path
+        Path[output_param].expand(Dir.current)
+      else
+        input_path.parent.join(input_path.stem)
+      end
+
+      unless Dir.exists?(output_directory)
+        Dir.mkdir(output_directory)
+      end
+
+      document = XML.parse(xml)
+
+      main_element = document.children.find do |child|
+        child.name == "svg" && child.element?
+      end
+
+      raise "SVG could not be parsed" if main_element.nil?
+
+      result =  main_element.children.select(&.element?).each do |child|
+        new_document = build_file(xml, child["id"])
+        if new_document
+          filename = child["id"].gsub(/[\/\\]/, "_") + ".svg"
+          output_path = output_directory.join(filename)
+          puts "Writing #{child["id"]} to #{output_path}"
+          write_file(new_document, output_path) unless dry_run
+        end
+      end
+    end
+
+    def write_file(content, path)
+      File.write(path, content)
+    end
+
+    # Ineffeciently build new document by deleting all svg children except those matching the ID parameter
+    def build_file(xml, id)
+      document = XML.parse(xml)
+
+      main_element = document.children.find do |child|
+        child.name == "svg" && child.element?
+      end
+      return nil unless main_element
+
+      main_element.children.each do |child|
+        if child["id"] != id
+          child.unlink
+        end
+      end
+      document.to_s
     end
   end
 end
